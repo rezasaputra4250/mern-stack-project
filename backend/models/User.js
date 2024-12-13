9947348f84083e353d40495e53d75054b8685298
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-12-13 11:36:43 +0700

    Menambahakan role pada model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  dateOfBirth: { type: Date, required: true },
-  gender: { type: String, enum: ['male', 'female', 'other'], required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  gender: { type: String, enum: ['male', 'female', 'other'], required: true },
+  role: {
+    type: String,
+    enum: ['student', 'teacher', 'admin'],
+    default: 'student',
+    required: true
+  },
 }, {
   timestamps: true,
   versionKey: false
